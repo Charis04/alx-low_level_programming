242cb698c54885785cdc45f9f3fd1d427a7db73a
Charis <charisadu@gmail.com>
2023-06-28 22:46:45 +0000

    print times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * times_table - prints the times table from 0 to 9
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int a, b, c, d, e;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			c = a * b;
+			if (c > 9)
+			{
+				d = c % 10;
+				e = (c - d) / 10;
+				_putchar(e + '0');
+				_putchar(d + '0');
+			}
+			else
+			{
+				_putchar(c + '0');
+			}
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
