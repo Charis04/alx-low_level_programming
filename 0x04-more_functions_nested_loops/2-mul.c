baa25bf111cbd840cde163f0def3741ab5db5d76
Charis <charisadu@gmail.com>
2023-07-10 15:31:53 +0000

    multiplies 2 integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+/**
+ * mul - multiplies 2 ints
+ * @a: first int to multiply
+ * @b: int to be multiplied with first int
+ * Return: product of multiplication
+ */
+
+int mul(int a, int b)
+{
+	int ret;
+
+	ret = a * b;
+	return (ret);
+}
