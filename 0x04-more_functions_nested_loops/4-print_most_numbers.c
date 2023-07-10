49af6b2c6a1a2ade41ed266a7cfe67e6d2724a2b
Charis <charisadu@gmail.com>
2023-07-10 21:38:25 +0000

    print numbers omiting 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * print_numbers - prints numbers from 0 to 9
+ * print_numbers - prints numbers from 0 to 9 omitting 2 and 4
  * Return: void
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	char i;
 
