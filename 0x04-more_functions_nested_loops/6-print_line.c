1620cf52c3f570f97f087b96a86ffa429b1608bd
Charis <charisadu@gmail.com>
2023-07-10 22:47:05 +0000

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line
+ * @n: number of "_" characters used to print the line
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		for (int i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
