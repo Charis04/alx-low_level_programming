b76a2739c47ee8bc18f0990235826b045188a191
Charis <charisadu@gmail.com>
2023-07-14 00:33:38 +0000

    copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/** _strcpy - copies one string into another
+ * @src: source of copy
+ * @dest: destination of copy
+ * Return: void
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, len = 0;
+
+	while (src[len] != '\0')
+		len++;
+	for (i = 0; i <= len; i++)
+		dest[i] = src[i];
+	return (dest);
+}
