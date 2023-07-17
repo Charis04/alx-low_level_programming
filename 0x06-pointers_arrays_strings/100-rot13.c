4d1814bde98d497613d0cd004bc4e2a4b6bad7fa
Charis <charisadu@gmail.com>
2023-07-17 19:13:09 +0000

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+/**
+ * rot13 - encodes a string using rot13 encryption
+ * @s: string to be encrypted
+ * Return: char
+ */
+
+char *rot13(char *s)
+{
+	int i, i2;
+	char alph[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i]; i++)
+	{
+		for (i2 = 0; i2 < 52; i2++)
+		{
+			if (s[i] == alph[i2])
+			{
+				s[i] = rot[i2];
+				break;
+			}
+		}
+	}
+	return (s);
+}
