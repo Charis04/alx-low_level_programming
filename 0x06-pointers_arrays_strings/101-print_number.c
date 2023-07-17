4c67c05a8d41ff5f4f8e694b155387a1bd9e2307
Charis <charisadu@gmail.com>
2023-07-17 21:14:09 +0000

    write func that prints ints

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,56 +7,40 @@
 
 void print_number(int n)
 {
-	int a, b, c, d, e, f, g, h, i, j;
+	int a, b, c;
+
+	if (n < 0)
+	{
+		n *= -1;
+		_putchar('-');
+	}
 
 	if (n > 999)
 	{
 		a = n % 1000;
-		b = (n - a) / 1000;
+		_putchar(n / 1000 + '0');
+		b = a % 100;
+		_putchar(a / 100 + '0');
+		c = b % 10;
+		_putchar(b / 10 + '0');
+		_putchar(c + '0');
+	}
+	else if (n > 99)
+	{
+		a = n % 100;
+		_putchar(n / 100 + '0');
+		b = a % 10;
+		_putchar(a / 10 + '0');
 		_putchar(b + '0');
-		if (a > 99)
-		{
-			c = a % 100;
-			d = (a - c) / 100;
-			_putchar(d + '0');
-			if (c > 9)
-			{
-				g = c % 10;
-				h = (c - g) / 10;
-				_putchar(h + '0');
-				_putchar(g + '0');
-			}
-			else
-			{
-				_putchar('0');
-				_putchar(c + '0');
-			}
-		}
-		else if (a > 9)
-		{
-			i = a % 10;
-			j = (a - i) / 10;
-			_putchar('0');
-			_putchar(j + '0');
-			_putchar(i + '0');
-		}
-		else
-		{
-			_putchar('0');
-			_putchar('0');
-			_putchar(a + '0');
-		}
 	}
 	else if (n > 9)
 	{
-		e = n % 10;
-		f = (n - e) / 10;
-		_putchar(f + '0');
-		_putchar(e + '0');
+		a = n % 10;
+		_putchar(n / 10 + '0');
+		_putchar(a + '0');
 	}
 	else
 	{
 		_putchar(n + '0');
 	}
-	_putchar('\n');
 }
