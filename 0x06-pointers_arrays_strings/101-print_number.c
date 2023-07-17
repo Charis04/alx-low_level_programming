e530685f8557d82d5962e1d90eb389ceb241175c
Charis <charisadu@gmail.com>
2023-07-17 21:39:26 +0000

    write func that prints ints

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,7 +8,6 @@
 
 void print_number(int n)
 {
-	/* My code wasn't optimal so i copied this one*/
 	unsigned int k = n;
 
 	if (n < 0)
@@ -21,42 +20,4 @@ void print_number(int n)
 	if (k != 0)
 		print_number(k);
 	_putchar((unsigned int) n % 10 + '0');
-
-	/* This is the code i wrote myself
-	int a, b, c;
-
-	if (n < 0)
-	{
-		n *= -1;
-		_putchar('-');
-	}
-
-	if (n > 999)
-	{
-		a = n % 1000;
-		_putchar(n / 1000 + '0');
-		b = a % 100;
-		_putchar(a / 100 + '0');
-		c = b % 10;
-		_putchar(b / 10 + '0');
-		_putchar(c + '0');
-	}
-	else if (n > 99)
-	{
-		a = n % 100;
-		_putchar(n / 100 + '0');
-		b = a % 10;
-		_putchar(a / 10 + '0');
-		_putchar(b + '0');
-	}
-	else if (n > 9)
-	{
-		a = n % 10;
-		_putchar(n / 10 + '0');
-		_putchar(a + '0');
-	}
-	else
-	{
-		_putchar(n + '0');
-	}*/
 }
