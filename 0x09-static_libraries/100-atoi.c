c33fae44e2af59fa8cf79493c8ee096ad18b60f7
Charis <charisadu@gmail.com>
2023-07-21 22:58:04 +0000

    create a static lib

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,30 @@
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ * Return: an int if there are numbers in the string and 0 if there are non
+ */
+
+int _atoi(char *s)
+{
+	int end = 0, i = 0, sign = 1;
+	unsigned int ret = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == 45)
+		{
+			sign *= -1;
+		}
+		while (s[i] >= 48 && s[i] <= 57)
+		{
+			end = 1;
+			ret = (ret * 10) + (s[i] - 48);
+			i++;
+		}
+		if (end == 1)
+			break;
+		i++;
+	}
+	ret *= sign;
+	return (ret);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an int
+ * @i: int to be computed
+ * Return: Absolute value
+ */
+
+int _abs(int i)
+{
+	int ret;
+
+	if (i < 0)
+	{
+		ret = i * -1;
+	}
+	else
+	{
+		ret = i * 1;
+	}
+	return (ret);
+}
