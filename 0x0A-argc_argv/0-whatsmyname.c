31f92fecdbee7a3d0721e865d69e93460b144cb2
Charis <charisadu@gmail.com>
2023-07-22 21:09:25 +0000

    print name of program to output

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of program
+ * @argc: number of command line arguements inputed
+ * @argv: command line arguements inputed
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
