6fa681187e9d833a02f2033b7723e72e8c3fe3db
Charis <charisadu@gmail.com>
2023-07-22 23:09:49 +0000

    print sum of nuum

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,36 +1,46 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
- * main - prints the sum of numbers
- * @argc: number of CL args
- * @argv: CL args
- * Return: 0
- */
-
+  * main - Prints the sum of args positive numbers
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: Always zero
+  */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i;
+	unsigned int k, sum = 0;
+	char *e;
 
-	if (argc < 1)
-	{
-		printf("%i\n", 0);
-	}
-	else
+	if (argc > 1)
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (atoi(argv[i]))
+			e = argv[i];
+
+			for (k = 0; k < strlen(e); k++)
 			{
-				sum += atoi(argv[i]);
+				if (e[k] < 48 || e[k] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
+
+			sum += atoi(e);
+			e++;
 		}
-		printf("%i\n", sum);
+
+		printf("%d\n", sum);
 	}
+	else
+	{
+		printf("0\n");
+	}
+
 	return (0);
 }
