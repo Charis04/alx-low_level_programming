195953ccfd488c87cb02ba93dd8f13ead6afbaa3
Charis <charisadu@gmail.com>
2023-07-22 22:30:28 +0000

    print product of 2 numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguements
+ * @argv: CL args
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum;
+
+	if (argc < 3)
+	{
+		printf("Error");
+	}
+	else
+	{
+		sum = atoi(argv[1]) + atoi(argv[2]);
+		printf("%i\n", sum);
+	}
+	return (0);
+}
