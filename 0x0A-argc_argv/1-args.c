d656342692479700cde95df57a39a12d92aa82b5
Charis <charisadu@gmail.com>
2023-07-22 21:15:21 +0000

    print number of args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguements
+ * @argc: number of CL arguements
+ * @argv: CL arguements
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%i\n", argc);
+	(void)argv[];
+	return (0);
+}
