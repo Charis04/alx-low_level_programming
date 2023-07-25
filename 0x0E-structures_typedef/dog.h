d3d579095237c811de1fedc65475a4127a0f73fa
Charis <charisadu@gmail.com>
2023-07-25 10:57:57 +0000

    create a struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * Struct dog - A structure that contains info about a dog
+ * @name: Name of dog
+ * @age: Dog's age
+ * @owner: Dog's owner
+ */
+
+Struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
