3e13229dd2ea4a7488f6040ab31070829e35b65b
Charis <charisadu@gmail.com>
2023-07-25 15:11:52 +0000

    Write func that creates new struct

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - create a new dog var
+ * @name: Name of new dog
+ * @age: Age of new dog
+ * @Owner: Owner of new dog
+ * Return: pointer to new dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	dog_t new;
+	dog = &new;
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
+	return (dog);
+}
