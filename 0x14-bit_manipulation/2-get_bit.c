270a6afa57f87fea41abfd15c410024fa73defe9
Charis <charisadu@gmail.com>
2023-09-02 13:46:25 +0000

    get bit at index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,7 +10,10 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int check = 1UL << index;
+	unsigned int maxi = (sizeof(ULONG_MAX) * 8 - 1);
 
+	if (index > maxi)
+		return (-1);
 	if (n & check)
 		return (1);
 	else
