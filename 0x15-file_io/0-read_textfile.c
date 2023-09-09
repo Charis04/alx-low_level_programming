f189576a9bbf6263e1e4b8b035275cf751661bd5
Charis <charisadu@gmail.com>
2023-09-09 00:08:59 +0000

    read text from file and print to stdout

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,38 +15,36 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-	fd = open(filename, O_RDWR);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
-		dprintf(STDERR_FILENO, "Failed to open file");
+		dprintf(STDERR_FILENO, "Failed to open file\n");
 		return (0);
 	}
 
-	buffer = malloc(sizeof(char) * letters + 1);
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
-		dprintf(STDERR_FILENO, "Failed to create buffer to read file to");
+		dprintf(STDERR_FILENO, "Failed to create buffer to read file to\n");
 		return (0);
 	}
-	buffer[letters] = '\0';
 
 	byts_rd = read(fd, buffer, letters);
 	if (byts_rd == -1)
 	{
-		dprintf(STDERR_FILENO, "Failed to read file");
+		dprintf(STDERR_FILENO, "Failed to read file\n");
 		return (0);
 	}
-	buffer[byts_rd] = '\0';
 
 	byts_wr = write(STDOUT_FILENO, buffer, byts_rd);
 	if (byts_wr == -1)
 	{
-		dprintf(STDERR_FILENO, "Failed to print text");
+		dprintf(STDERR_FILENO, "Failed to print text\n");
 		return (0);
 	}
 	if (byts_wr != byts_rd)
 	{
-		dprintf(STDERR_FILENO, "Failed to print expected number of bytes");
+		dprintf(STDERR_FILENO, "Failed to print expected number of bytes\n");
 		return (0);
 	}
 	free(buffer);
