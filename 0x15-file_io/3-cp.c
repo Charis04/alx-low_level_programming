9ea7ba6934de782c101fbec5aac48776535901e3
Charis <charisadu@gmail.com>
2023-09-09 21:08:16 +0000

    copy contents of a file to another

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,78 @@
+#include "main.h"
+
+void cp_content(const char *src, const char *dest);
+
+/**
+ * main - copies contents of one file to another
+ * @ac: arg count
+ * @av: arg vector = src file and dest file
+ * Return: 0
+ */
+
+int main(int ac, char **av)
+{
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	cp_content(av[1], av[2]);
+
+	return (0);
+}
+
+/**
+ * cp_content - copies contents of one file to another
+ * @src: source file
+ * @dest: destination file
+ * Return: void
+ */
+
+void cp_content(const char *src, const char *dest)
+{
+	int srcfd, destfd, close1, close2;
+	ssize_t bytsrd, bytswr;
+	char buffer[1024];
+
+	srcfd = open(src, O_RDONLY);
+	if (srcfd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
+		exit(98);
+	}
+
+	bytsrd = read(srcfd, buffer, 1024);
+	if (bytsrd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
+		exit(98);
+	}
+
+	destfd = open(dest, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (destfd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest);
+		exit(99);
+	}
+
+	bytswr = write(destfd, buffer, 1024);
+	if (bytswr == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest);
+		exit(99);
+	}
+
+	close1 = close(srcfd);
+	if (close1 == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d", srcfd);
+		exit(100);
+	}
+	close2 = close(destfd);
+	if (close2 == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d", destfd);
+		exit(100);
+	}
+}
