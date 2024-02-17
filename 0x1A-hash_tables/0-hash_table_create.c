bbda5d52faee03a9b4f2b084d55da826dbeeee80
Charis <charisadu@gmail.com>
2024-02-17 23:16:58 +0000

    fix bugs in hash_table_create

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,18 +3,15 @@
 /**
  * hash_table_create - creates a hash table
  * @size: size of the hash table
- * 
+ *
  * Return: a pointer to the hash table
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *ht = (hash_table_t *) malloc(sizeof(hash_table_t));
-    ht->size = size;
-    ht->array = (hash_node_t *) malloc(sizeof(hash_node_t *));
-    ht->array->key = (char *) malloc(sizeof(char *));
-    ht->array->value = (char *) malloc(sizeof(char *));
-    ht->array->next = NULL
-
-    return (ht);
+	hash_table_t *ht = (hash_table_t *) malloc(sizeof(hash_table_t));
+	ht->size = size;
+	ht->array = (hash_node_t **) malloc(sizeof(hash_node_t *));
+	
+	return (ht);
 }
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -6,9 +6,6 @@
 #include <string.h>
 #include <stdio.h>
 
-/* Function Prototypes */
-hash_table_t *hash_table_create(unsigned long int size);
-
 /* Data Structures */
 
 /**
@@ -19,12 +16,13 @@ hash_table_t *hash_table_create(unsigned long int size);
  * @value: The value corresponding to a key
  * @next: A pointer to the next node of the List
  */
+
 typedef struct hash_node_s
 {
 	char *key;
 	char *value;
 	struct hash_node_s *next;
-} hash_node_t;
+}hash_node_t;
 
 /**
  * struct hash_table_s - Hash table data structure
@@ -34,10 +32,14 @@ typedef struct hash_node_s
  * Each cell of this array is a pointer to the first node of a linked list,
  * because we want our HashTable to use a Chaining collision handling
  */
+
 typedef struct hash_table_s
 {
 	unsigned long int size;
 	hash_node_t **array;
-} hash_table_t;
+}hash_table_t;
+
+/* Function Prototypes */
+hash_table_t *hash_table_create(unsigned long int size);
 
 #endif
