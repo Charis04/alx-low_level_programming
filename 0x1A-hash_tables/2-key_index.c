919d7375a1ddc21370d7b19162f1542e1f0b6243
Charis <charisadu@gmail.com>
2024-02-18 14:23:06 +0000

    create func that gets index of key

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,18 @@
+#include "hash_table.h"
+
+/**
+ * key_index - gives the index of a key
+ * @key: key whose index is to be found
+ * @size: size of the hash table
+ * 
+ * Return: the index of the key
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index;
+
+	index - hash_djb2(key) % size;
+
+	return (index);
+}
