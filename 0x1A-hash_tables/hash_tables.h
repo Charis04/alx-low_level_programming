0c42f7a98c2560b6a3fdee69724595e1cae54eea
Charis <charisadu@gmail.com>
2024-02-18 00:06:26 +0000

    fix betty coding style

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -22,7 +22,7 @@ typedef struct hash_node_s
 	char *key;
 	char *value;
 	struct hash_node_s *next;
-}hash_node_t;
+} hash_node_t;
 
 /**
  * struct hash_table_s - Hash table data structure
@@ -37,7 +37,7 @@ typedef struct hash_table_s
 {
 	unsigned long int size;
 	hash_node_t **array;
-}hash_table_t;
+} hash_table_t;
 
 /* Function Prototypes */
 hash_table_t *hash_table_create(unsigned long int size);
