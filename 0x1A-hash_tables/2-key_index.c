5604a5d9ea55a39569d85cc9e5e620a77e1db5b3
Charis <charisadu@gmail.com>
2024-02-18 14:28:04 +0000

    create main file to test key to index func

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,10 +1,10 @@
-#include "hash_table.h"
+#include "hash_tables.h"
 
 /**
  * key_index - gives the index of a key
  * @key: key whose index is to be found
  * @size: size of the hash table
- * 
+ *
  * Return: the index of the key
  */
 
@@ -12,7 +12,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int index;
 
-	index - hash_djb2(key) % size;
+	index = hash_djb2(key) % size;
 
 	return (index);
 }
