39e1296da94f00e1ac350ca12dd969e7e7d81456
Charis <charisadu@gmail.com>
2024-05-23 04:55:03 +0100

    implememnt lin search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+
+
+/**
+ * linear_search - Searches an array linearly
+ *
+ * @array: Pointer to the integer array to search
+ * @size: Size of the array
+ * @value: The value to search for
+ *
+ * Return: The index of the first occurrence of the value in the array,
+ *          or -1 if not found
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+			return ((int )i);
+	}
+	return (-1);
+}
