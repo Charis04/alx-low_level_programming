4cc57908cdda4f4f5bdd4eb0672437ec4815905e
Charis <charisadu@gmail.com>
2024-05-24 02:23:26 +0100

    implement bin search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,7 +1,5 @@
 #include "search_algos.h"
 
-void print_array(int *arr, size_t size);
-
 /**
  * binary_search - search an array using binary search
  * @array: pointer to first element of array to be searched
@@ -12,43 +10,24 @@ void print_array(int *arr, size_t size);
 
 int binary_search(int *array, size_t size, int value)
 {
-	size_t l = 0, r = size - 1, mid = (l + r) / 2;
+	size_t l = 0, r = size - 1, mid, i;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
 
 	while (l <= r)
 	{
-		print_array(&array[l], r - l + 1);
+		printf("Searching in array: %d", array[l]);
+		for (i = l + 1; i <= r; i++)
+			printf(", %d", array[i]);
+		printf("\n");
+		mid = l + r / 2;
 		if (array[mid] == value)
 			return ((int)mid);
 		else if (array[mid] < value)
 			l = mid + 1;
 		else
 			r = mid - 1;
-		mid = l + r / 2;
 	}
 	return (-1);
-}
-
-/**
- * print_array - prints the elements of an array
- * @arr: pointer to first element of array to be printed
- * @size: size of the array
-*/
-
-void print_array(int *arr, size_t size)
-{
-	size_t i = 0;
-
-	if (arr == NULL)
-		return;
-
-	printf("Searching in array: %d", arr[i]);
-	for (i = 1; i < size; i++)
-	{
-		printf("i is %lu and size is %lu", i, size);
-		printf(", %d", arr[i]);
-	}
-	printf("\n");
 }
